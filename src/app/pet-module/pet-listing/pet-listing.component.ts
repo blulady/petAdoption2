1731b80607f04fbb70a5d1850380cd8aab7beb02
Lucile Jones <jones.lucile@gmail.com>
2023-11-30 13:31:23 -0700

    initial setup

---
+++ src/app/pet-module/pet-listing/pet-listing.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pet-listing',
+  templateUrl: './pet-listing.component.html',
+  styleUrls: ['./pet-listing.component.css']
+})
+export class PetListingComponent {
+
+}
