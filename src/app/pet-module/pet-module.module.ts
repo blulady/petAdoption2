7aa4968b4376e8528c38dd7de7c58a837ed59b8c
ssanger <ssanger2020@gmail.com>
2023-12-04 10:23:50 -0800

    added auth & home modules to complete routing

--- src/app/pet-module/pet-module.module.ts
+++ src/app/pet-module/pet-module.module.ts
@@ -4,12 +4,16 @@ import { CommonModule } from '@angular/common';
 import { PetModuleRoutingModule } from './pet-module-routing.module';
 import { PetListingComponent } from './pet-listing/pet-listing.component';
 import { PetDetailsComponent } from './pet-details/pet-details.component';
+import { PetListComponent } from './pet-list/pet-list.component';
+import { PetDetailComponent } from './pet-detail/pet-detail.component';
 
 
 @NgModule({
   declarations: [
     PetListingComponent,
-    PetDetailsComponent
+    PetDetailsComponent,
+    PetListComponent,
+    PetDetailComponent
   ],
   imports: [
     CommonModule,
