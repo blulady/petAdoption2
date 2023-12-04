8479eded782cbb3ae8e24f3aafd2f396d6df1c42
ssanger <ssanger2020@gmail.com>
2023-12-04 10:19:05 -0800

    added routes to modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeModule } from './home/home.module';
+import { PetModuleModule } from './pet-module/pet-module.module';
+import { AuthModule } from './auth/auth.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: "full"},
+  {path: 'home', component: HomeModule},
+  {path: 'auth', component: AuthModule},
+  {path: 'pet', component: PetModuleModule}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
