0eb12ec2eb2dd9a04c58de53e7cda57243728509
ssanger <ssanger2020@gmail.com>
2023-12-04 17:47:37 -0800

    cleaned up pet-module.module.ts from duplicate modules

--- src/app/pet-module/pet-module.module.ts
+++ src/app/pet-module/pet-module.module.ts
@@ -4,16 +4,12 @@ import { CommonModule } from '@angular/common';
 import { PetModuleRoutingModule } from './pet-module-routing.module';
 import { PetListingComponent } from './pet-listing/pet-listing.component';
 import { PetDetailsComponent } from './pet-details/pet-details.component';
-import { PetListComponent } from './pet-list/pet-list.component';
-import { PetDetailComponent } from './pet-detail/pet-detail.component';
 
 
 @NgModule({
   declarations: [
     PetListingComponent,
     PetDetailsComponent,
-    PetListComponent,
-    PetDetailComponent
   ],
   imports: [
     CommonModule,
