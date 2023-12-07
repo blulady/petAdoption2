1c9ea6cd2ae046777b8be5b1bb8c3441d8a4d081
Norbok7 <72887701+Norbok7@users.noreply.github.com>
2023-12-07 14:32:59 -0600

    more animals!

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -3,6 +3,10 @@ import { PetService } from 'src/app/pet-module/pet.service';
 const catData = [
   { name: 'Tabby', description: 'A crazy feline', species: 'cat', img: 'https://i.pinimg.com/736x/b0/3c/be/b03cbeaded6326b85d1edb86c91caa59.jpg' },
   { name: 'Twinkels', description: 'is a large hairless cat', species: 'cat',  img: 'https://justintadlock.com/user/media/2023/07/twinkle-cat-bed.webp' },
+  { name: 'Tabby', description: 'A crazy feline', species: 'cat', img: 'https://i.pinimg.com/736x/b0/3c/be/b03cbeaded6326b85d1edb86c91caa59.jpg' },
+  { name: 'Twinkels', description: 'is a large hairless cat', species: 'cat',  img: 'https://justintadlock.com/user/media/2023/07/twinkle-cat-bed.webp' },
+  { name: 'Tabby', description: 'A crazy feline', species: 'cat', img: 'https://i.pinimg.com/736x/b0/3c/be/b03cbeaded6326b85d1edb86c91caa59.jpg' },
+  { name: 'Twinkels', description: 'is a large hairless cat', species: 'cat',  img: 'https://justintadlock.com/user/media/2023/07/twinkle-cat-bed.webp' },
 
 ];
 
@@ -10,7 +14,12 @@ const dogData = [
   { name: 'Spot', description: 'Is a good boy', species: 'dog', img: 'https://sp-ao.shortpixel.ai/client/to_auto,q_glossy,ret_img,w_700,h_467/https://puppyintraining.com/wp-content/uploads/Black-and-White-Dog-Names-700x467.jpg' },
   { name: 'Rufus', description: 'Is a bad boy', species: 'dog',  img: 'https://www.rocketdogrescue.org/wp-content/uploads/2018/06/IMG_9025-1.jpg' },
   { name: 'Nolan', description: 'Is a genius dog', species: 'dog', img: 'https://thevillagevets.com/wp-content/uploads/2022/10/most-intelligent-dog-breeds-atlanta-ga.jpg'},
-  // Add more dog data as needed
+  // Add more dog data as needed{ name: 'Spot', description: 'Is a good boy', species: 'dog', img: 'https://sp-ao.shortpixel.ai/client/to_auto,q_glossy,ret_img,w_700,h_467/https://puppyintraining.com/wp-content/uploads/Black-and-White-Dog-Names-700x467.jpg' },
+  { name: 'Rufus', description: 'Is a bad boy', species: 'dog',  img: 'https://www.rocketdogrescue.org/wp-content/uploads/2018/06/IMG_9025-1.jpg' },
+  { name: 'Nolan', description: 'Is a genius dog', species: 'dog', img: 'https://thevillagevets.com/wp-content/uploads/2022/10/most-intelligent-dog-breeds-atlanta-ga.jpg'},
+  { name: 'Spot', description: 'Is a good boy', species: 'dog', img: 'https://sp-ao.shortpixel.ai/client/to_auto,q_glossy,ret_img,w_700,h_467/https://puppyintraining.com/wp-content/uploads/Black-and-White-Dog-Names-700x467.jpg' },
+  { name: 'Rufus', description: 'Is a bad boy', species: 'dog',  img: 'https://www.rocketdogrescue.org/wp-content/uploads/2018/06/IMG_9025-1.jpg' },
+  { name: 'Nolan', description: 'Is a genius dog', species: 'dog', img: 'https://thevillagevets.com/wp-content/uploads/2022/10/most-intelligent-dog-breeds-atlanta-ga.jpg'},
 ];
 @Component({
   selector: 'app-home',
