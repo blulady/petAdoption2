add980992da1a667abf845b7b8cfee31b507f55a
ssanger <ssanger2020@gmail.com>
2023-12-11 11:13:16 -0800

    completed sign in & need to update local

--- src/app/auth/auth/auth.component.ts
+++ src/app/auth/auth/auth.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import {NgForm } from '@angular/forms';
+import { AuthResponseData, AuthService } from '../auth.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-auth',
@@ -7,18 +9,43 @@ import {NgForm } from '@angular/forms';
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent {
-  isLoginMode = true;
+  isLoginMode = false;
+  success_message: string | null = null;
+  error: string | null = null;
+  constructor(private authService: AuthService) {}
+
   onCreateAccount() {
     this.isLoginMode = false;
   }
   onLogin (form: NgForm) {
-    this.isLoginMode = true;
+    this.isLoginMode = false;
     form.reset();
   }
 
 onSubmit(form: NgForm) {
-  console.log(form.value)
-}
+  if (!form.valid) {
+    return;}
+  const email = form.value.email;
+  const password = form.value.password;
+
+  let authObs: Observable<AuthResponseData>;
+
+  if (this.isLoginMode) {
+    authObs = this.authService.login(email, password);
+  } else {
+    authObs = this.authService.signup(email, password);}
+
+    authObs.subscribe(resData => {console.log(resData);},
+    errorRes => {console.log(errorRes);
+    this.error = errorRes.error.error.message;})
+    if (!this.error) {
+      this.success_message = "Account Successfully Created."
+    }
+    console.log(this.error)
+    console.log(this.success_message);
+    form.reset()
+    }
+
 
 }
 
