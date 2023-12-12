ea8fc986d2a86c3abe0f5a64847d0e2dc7a9d99d
ssanger <ssanger2020@gmail.com>
2023-12-11 22:35:15 -0800

    login & logout working

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './auth/auth.component';
+import { AuthInterceptorService } from './auth-interceptor.service';
+
 
 
 
@@ -13,7 +16,9 @@ import { AuthComponent } from './auth/auth.component';
   imports: [
     CommonModule,
     FormsModule,
+    HttpClientModule,
     AuthRoutingModule
-  ]
+  ],
+  // providers: [{provide:HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true }]
 })
 export class AuthModule { }
