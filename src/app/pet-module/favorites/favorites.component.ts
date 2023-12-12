4a2270d527a7e01eef608c3eac6b01ffefa079b2
Norbok7 <72887701+Norbok7@users.noreply.github.com>
2023-12-12 12:30:58 -0600

    favorites component styling editing,

--- src/app/pet-module/favorites/favorites.component.ts
+++ src/app/pet-module/favorites/favorites.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FavoritePetModel } from '../petmodel';
-import { PetService } from '../pet.service';
+import { DataStorageFirebase } from 'src/app/shared/data-storage-firebase.service';
+import { FavoritePetModel } from 'src/app/pet-module/petmodel';
+import { PetService } from 'src/app/pet-module/pet.service';
+import { Router } from '@angular/router'; // Import Router
 
 @Component({
   selector: 'app-favorites',
@@ -10,9 +12,15 @@ import { PetService } from '../pet.service';
 export class FavoritesComponent implements OnInit {
   myFavorites: FavoritePetModel[] = [];
 
-  constructor(private petService: PetService) { }
+  constructor(private petService: PetService, private router: Router, data: DataStorageFirebase) { }
 
   ngOnInit(): void {
     this.myFavorites = this.petService.getFavorites();
   }
+
+   // Navigate to detail view
+   goToDetail(id: number): void {
+    this.router.navigate(['/pet', id]);
+  }
 }
+
